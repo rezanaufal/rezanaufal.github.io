212d84b56ea307161eefa89746deaca74562cc20
Reza Yusril Naufal <10191071@student.itk.ac.id>
2021-05-11 22:36:29 +0800

    qer

--- resources/views/dashboard/landing.blade.php
+++
@@ -1,9 +0,0 @@
-@extends('layout.dash_layout')
-@section('title. content')
-@section('content')
-
-<div style="text-align: center">
-    <h1 style="margin-top: 250px">Welcome to <br> <b>Admin Dashboard </b> </h1>
-    
-</div>
-@endsection
